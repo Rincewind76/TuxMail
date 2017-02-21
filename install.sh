da9f058c0f7294c8193f0feeddbde00919337363
Thomas Schödl <thomas@schoedl.me>
2017-02-21 15:49:56 +0100

    start of instalaltion script

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-echo ************************************************************************************
-echo ** Install Mail server on Ubuntu with Dovecot, postfix, SpamAssassin and MySQL
-echo ************************************************************************************
 echo
-read -p "Press Enter to continue"
+echo "************************************************************************************"
+echo "************************************************************************************"
+echo "** Install Mail server on Ubuntu with Dovecot, postfix, SpamAssassin and MySQL"
+echo "************************************************************************************"
+echo "************************************************************************************"
+echo "This installation is following the tutorial of Thomas Leister found here:"
+echo "https://thomas-leister.de/mailserver-unter-ubuntu-16.04/"
+echo
+echo "You should use this setup on a fresh installation of Ubuntu"
+echo
+read -p "Press Enter to continue or Ctrl+C to abort"
 
 echo "test"
 
