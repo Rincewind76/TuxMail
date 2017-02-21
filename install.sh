567f3dd276ba2360fcc2d306da806c19c234eb3b
Rincewind76 <thomas@schoedl.me>
2017-02-21 16:43:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+echo ************************************************************************************
+echo ** Install Mail server on Ubuntu with Dovecot, postfix, SpamAssassin and MySQL
+echo ************************************************************************************
+echo
+read -p "Press Enter to continue"
+
+echo "test"
+
 
 exit 0
